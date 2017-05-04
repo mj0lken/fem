9930f33c8ec9031fec73d248e2b45cf61c03b6ff
Erik <erikzwolff@gmail.com>
2017-05-04 15:59:31 +0200

    Mesh is made using PDEtool

---
+++ project.m
@@ -0,0 +1,15 @@
+%% Project
+
+
+load('edges.mat');
+load('points.mat');
+load('triangles.mat');
+
+nelm=length(t(1,:)); %Counts the number of triangles. Which is nelm
+edof(:,1)=1:nelm; % element 1-188, first column of edof
+edof(:,2:4)=t(1:3,:)'; % 2-4 column of edof is the same as row 1-3 in t
+coord=p'; % Coord = all the points and their coordinates
+ndof=max(max(t(1:3,:))) ; % max(t(1:3,:)) gives max values in row 1-3 
+%and second max gives total max (Number of nodes)
+[Ex,Ey]=coordxtr(edof,coord,(1:ndof)',3);
+eldraw2(Ex,Ey,[1,4,1],edof(:,1));
